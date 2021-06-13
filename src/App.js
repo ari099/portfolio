ee7a43913b385aef1a69ed253cf99da091de08f9
Ariel Lorenzo <ariellorenzo08@gmail.com>
2021-06-13 19:55:41 -0400

    Added bottom navigation on page

--- src/App.js
+++ src/App.js
@@ -12,27 +12,65 @@ class Page extends Component {
   constructor(props) {
     super(props);
     this.letterColors = {
-      'A': 'blue',
+      'A': '#06F',
       'R': 'red',
       'I': 'gray'
     };
+    this.state = {
+      current: this.props.current
+    };
+    this.switchPages = this.switchPages.bind(this);
   }
 
   componentDidMount() {
-    anime({
+    let startPage = anime.timeline({
+      easing: 'easeInOutQuad',
+      duration: 750
+    });
+
+    startPage.add({
       targets: '.Page',
       opacity: [0, 1],
-      border: `1px solid ${this.letterColors[this.props.current]}`,
-      easing: 'easeInOutQuad',
-      duration: 1000
+      height: ['0%', '75%']
     });
+    startPage.add({
+      targets: '.Page-mini-nav',
+      opacity: [0, 1]
+    });
+  }
+
+  /**
+   * Switch pages from the small navigation bar at the bottom of the page
+   * When the user click a circle, it will light it up and revert the previous
+   * colored circle to a regular one
+   */
+  switchPages(letter) {
+    this.setState({ current: letter });
   }
 
   render() {
     return (
       <div className="Page">
+        <div className="Page-view"></div>
         <FontAwesomeIcon className="Page-controls"
           icon={faTimes} onClick={this.props.closePage} />
+        <svg className="Page-mini-nav">
+          <circle className="Page-nav-button navA"
+            cx="11.25" cy="12.5"
+            onClick={() => this.switchPages('A')}
+            r={(this.state.current === 'A') ? 7.5 : 5}
+            fill={(this.state.current === 'A') ? this.letterColors[this.state.current] : 'black'} />
+          <circle className="Page-nav-button navR"
+            cx="32.5" cy="12.5"
+            onClick={() => this.switchPages('R')}
+            r={(this.state.current === 'R') ? 7.5 : 5}
+            fill={(this.state.current === 'R') ? this.letterColors[this.state.current] : 'black'} />
+          <circle className="Page-nav-button navI"
+            cx="52.25" cy="12.5"
+            onClick={() => this.switchPages('I')}
+            r={(this.state.current === 'I') ? 7.5 : 5}
+            fill={(this.state.current === 'I') ? this.letterColors[this.state.current] : 'black'} />
+        </svg>
       </div>
     );
   }
@@ -61,6 +99,7 @@ class App extends Component {
     anime({
       targets: '.Page',
       opacity: [1, 0],
+      height: ['75%', '0%'],
       easing: 'easeInOutQuad',
       duration: 500,
       complete: () => {
