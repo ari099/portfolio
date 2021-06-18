6700988b4285d5671c2412aa86977af1fd191bce
Ariel Lorenzo <ariellorenzo08@gmail.com>
2021-06-18 08:58:32 -0400

    Began adding profile background

--- src/About.js
+++ src/About.js
@@ -18,15 +18,8 @@ class About extends Component {
   render() {
     return (
       <>
-        <div className="About-page-left">
-          <img className="About-profile-pic" src={ari} />
-        </div>
-        <div className="About-page-right">
-          <div className="About-page-content">
-            <p>I am a full-stack developer with about 3 years of experience in the field. I have worked mostly with educational institutes and collaborations with other developers and designers.</p>
-            <p>I try my best to keep track of the changing trends in web and mobile development to always stay current.</p>
-          </div>
-        </div>
+        <div className="About-profile-top-background"></div>
+        <img className="About-profile-pic" src={ari} />
       </>
     );
   }
